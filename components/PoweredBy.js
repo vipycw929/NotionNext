463a2f025e80c33e807f8441a8fd314672d58c82
vipycw929 <vipycw929@gmail.com>
2026-07-17 05:02:07 +0800

    Edit footer
    Edit footer(Powered byNotionNext 4.10.6.)

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -5,15 +5,23 @@ import { siteConfig } from '@/lib/config'
  * @returns
  */
 export default function PoweredBy(props) {
+  // 已自定义版权信息，原始代码已注释
   return (
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
-      <span className='mr-1'>Powered by</span>
-      <a
-        href='https://github.com/tangly1024/NotionNext'
-        className='underline justify-start'>
-        NotionNext {siteConfig('VERSION')}
-      </a>
-      .
+      © 2026 我的博客
     </div>
   )
+
+  // 以下为原始代码（已注释）
+  // return (
+  //   <div className={`inline text-sm font-serif ${props.className || ''}`}>
+  //     <span className='mr-1'>Powered by</span>
+  //     <a
+  //       href='https://github.com/tangly1024/NotionNext'
+  //       className='underline justify-start'>
+  //       NotionNext {siteConfig('VERSION')}
+  //     </a>
+  //     .
+  //   </div>
+  // )
 }
