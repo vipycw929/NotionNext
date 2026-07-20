523ff147b90039bfc7f1bb261b48fa11ff17e096
vipycw929 <vipycw929@gmail.com>
2026-07-20 19:28:41 +0800

    del by notion
    Removed the powered by NotionNext section from the footer.

--- themes/simple/components/Footer.js
+++ themes/simple/components/Footer.js
@@ -34,14 +34,6 @@ export default function Footer(props) {
             </a>
           )}
           <BeiAnGongAn />
-         <span className='no-underline ml-4'>
-          Powered by
-          <a
-            href='https://github.com/tangly1024/NotionNext'
-            className=' hover:underline'>
-            NotionNext {siteConfig('VERSION')}
-          </a>
-        </span>
         {/* 
         <span className='no-underline ml-4'>
           Powered by
